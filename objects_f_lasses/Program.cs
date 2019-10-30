c471b8cb3ac3c3665c7b4cd5165603293ea013ef
Comp0709 <Comp0709@tehnikum.ket.local>
2019-10-30 13:13:51 +0300

    objects_of_classes

---
+++ objects_f_lasses/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objects_f_lasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User { name = "Tom", age = 22 };
+            ChangeUser(ref user1);
+            Console.WriteLine($"{user1.name}-{user1.age}");
+            Console.ReadKey();
+
+        }
+        static void ChangeUser(ref User user)
+        {
+            user.name = "Bob";
+            user.age = 23;
+        }
+    }
+    struct User
+    {
+        public int age;
+        public string name;
+    }
+    
+    class Person
+    {
+        public int age;
+        public string name;
+    }
+}
